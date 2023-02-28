d20335bc02a18b632a0086afecf1ee5b5cd34016
FerrandezBenjamin <58741067+FerrandezBenjamin@users.noreply.github.com>
2023-02-28 08:36:07 +0100

    card OK

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,63 @@
 @section('title', 'Liste des groupes')
 
 @section('content')
-<h1>Bienvenue</h1>
+<div class="container">
+
+    <div class="containerCard">
+
+        <div class="card">
+            <label for="card1"></label>
+            <div class="cardContainer">
+                <h2> Name of Groupe </h2>
+                <div class="dataCard">
+                    <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit
+                        Itaque eos impedit doloremque possimus ea odio, similique dicta ad
+                        est quae maiores officia repellendus nostrum facilis eaquNihil, quam placeat.e temporibus!
+                    </span>
+                </div>
+                <div class="join">
+                    <button>Rejoindre ce groupe ! </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <label for="card1"></label>
+            <div class="cardContainer">
+                <h2> Name of Groupe </h2>
+                <div class="dataCard">
+                    <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit
+                        Itaque eos impedit doloremque possimus ea odio, similique dicta ad
+                        est quae maiores officia repellendus nostrum facilis eaquNihil, quam placeat.e temporibus!
+                    </span>
+                </div>
+                <div class="join">
+                    <button>Rejoindre ce groupe ! </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <label for="card1"></label>
+            <div class="cardContainer">
+                <h2> Name of Groupe </h2>
+                <div class="dataCard">
+                    <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit
+                        Itaque eos impedit doloremque possimus ea odio, similique dicta ad
+                        est quae maiores officia repellendus nostrum facilis eaquNihil, quam placeat.e temporibus!
+                    </span>
+                </div>
+                <div class="join">
+                    <button>Rejoindre ce groupe ! </button>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+</div>
+
+
 
 @endsection
