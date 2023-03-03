777ea8e9299fe61c8a3ebea73d7d9575055c7422
FerrandezBenjamin <58741067+FerrandezBenjamin@users.noreply.github.com>
2023-03-03 19:32:28 +0100

    change migration, not end

--- app/Models/RoleUser.php
+++ app/Models/RoleUser.php
@@ -2,18 +2,12 @@
 
 namespace App\Models;
 
-use \App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Role extends Model
+class RoleUser extends Model
 {
     use HasFactory;
 
     public $timestamps = false;
-
-    public function users()
-    {
-        return $this->belongsToMany(User::class);
-    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Groupe::factory(5)->create();
-        // \App\Models\User::factory(30)->create();
+        // \App\Models\Groupe::factory(5)->create();
+        // \App\Models\Role::factory(1)->create();
+        \App\Models\RoleUser::factory(1)->create();
+        // \App\Models\User::factory(28)->create();
     }
 }
