c65a328a88f471ccc2e5d7e7435bdf740640513f
Benjamin <ferrandez.73000@yahoo.fr>
2023-03-16 20:31:11 +0100

    Creation de test User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,24 +12,14 @@ class UserFactory extends Factory
      *
      * @return array
      */
-    // public function definition()
-    // {
-    //     return [
-    //         'name' => $this->faker->name(),
-    //         'email' => $this->faker->unique()->safeEmail(),
-    //         'email_verified_at' => now(),
-    //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-    //         'remember_token' => Str::random(10),
-    //     ];
-    // }
-
     public function definition()
     {
         return [
-            'name' => 'AdminApp',
-            'email' => 'admin@admin.fr',
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$hQNpLpqmuQ3LjWPfHFPxJuxHU5hJNK3pSV7grnWKUOjK8kmDfEf1C', // password
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
         ];
     }
 
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,19 +3,53 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
+use App\Models\User;
 
-class ExampleTest extends TestCase
+class LoginRegister extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_access_page_login()
     {
-        $response = $this->get('/');
+        
+
+        $response = $this->get('/login');
 
         $response->assertStatus(200);
+    }
+
+    public function test_access_page_register()
+    {
+    
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_create_user_connecte_then()
+    {
+    
+        $user = User::factory(1)->create()->first();
+
+        $response = $this->actingAs($user)->get('/');
+    
+        $response->assertStatus(200);
+    }
+
+    public function test_delete_last_user()
+    {
+    
+        $user = User::latest()->first();
+
+        $response = $this->actingAs($user)
+                         ->get('/');
+    
+        $response->assertStatus(200);
+        $user->delete();
     }
 }
